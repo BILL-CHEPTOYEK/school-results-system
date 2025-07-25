60c7b22f0eb48112cd08d661f2981694318fa1da
Bill <billcheptoyek60@gmail.com>
2025-07-25 16:29:24 +0300

    user-module

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,8 +1,11 @@
+from .views import UserViewSet, RoleViewSet, PermissionViewSet, UserPermissionViewSet, RolePermissionViewSet
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, RoleViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'roles', RoleViewSet)
+router.register(r'permissions', PermissionViewSet)
+router.register(r'user-permissions', UserPermissionViewSet)
+router.register(r'role-permissions', RolePermissionViewSet)
 
 urlpatterns = router.urls
