dfcf52c1750fbc7f40a64234ffd70e2bda20b84e
Bill <billcheptoyek60@gmail.com>
2025-07-25 15:52:46 +0300

    results-module

--- backend/tenant_core/settings.py
+++ backend/tenant_core/settings.py
@@ -23,7 +23,7 @@ TENANT_APPS = [
     'apps.core',
     'apps.users',
     'apps.students',
-    'apps.r',
+    'apps.re',
     # Add other tenant-specific apps here, e.g. 'users', 'students', etc.
 ]
 
