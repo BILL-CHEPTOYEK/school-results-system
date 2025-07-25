e0ac88f98998dd5d17bd39897e9ecb24d97fc9bc
Bill <billcheptoyek60@gmail.com>
2025-07-25 16:16:48 +0300

    auth-module

--- backend/tenant_core/urls.py
+++ backend/tenant_core/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/auth/', include('apps.users.jwt_urls')),
     path('api/', include('apps.users.urls')),
     path('api/', include('apps.students.urls')),
     path('api/', include('apps.results.urls')),
