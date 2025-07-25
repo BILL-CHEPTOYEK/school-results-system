e5de5c6e653804ceb1fd1e9a5039793cfc93a3b1
Bill <billcheptoyek60@gmail.com>
2025-07-25 16:16:42 +0300

    auth-module

--- backend/tenant_core/settings.py
+++ backend/tenant_core/settings.py
@@ -125,3 +125,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Custom user model
 AUTH_USER_MODEL = 'users.User'
+
+# DRF and JWT Authentication
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
