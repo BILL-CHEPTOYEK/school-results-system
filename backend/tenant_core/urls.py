d5465549515974de3a2b02a3ee5f58956637abe0
Bill <billcheptoyek60@gmail.com>
2025-07-26 12:09:09 +0300

    backend

--- backend/tenant_core/urls.py
+++ backend/tenant_core/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +27,6 @@ urlpatterns = [
     path('api/', include('apps.results.urls')),
     path('api/', include('apps.reports.urls')),
     path('api/analytics/', include('apps.analytics.urls')),
+    path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
