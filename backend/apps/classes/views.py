725ebf92600f014ed3e45e847e994e8c1f222501
Bill <billcheptoyek60@gmail.com>
2025-08-01 13:01:16 +0300

    added classes page

--- backend/apps/classes/views.py
+++ backend/apps/classes/views.py
@@ -5,3 +5,5 @@ from .serializers import ClassSerializer
 class ClassViewSet(viewsets.ModelViewSet):
     queryset = Class.objects.all()
     serializer_class = ClassSerializer
+    from rest_framework.permissions import IsAuthenticated
+    permission_classes = [IsAuthenticated]
