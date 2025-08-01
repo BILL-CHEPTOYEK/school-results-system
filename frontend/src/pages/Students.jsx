a98feb59e07c2f509e832d9a252e1f96d5cf633a
Bill <billcheptoyek60@gmail.com>
2025-08-01 10:16:52 +0300

    students

--- frontend/src/pages/Students.jsx
+++ frontend/src/pages/Students.jsx
@@ -69,7 +69,7 @@ export default function Students() {
                     value={selectedClass}
                     onChange={e => setSelectedClass(e.target.value)}
                 >
-                    <option value="">-- Choose a class/cour --</option>
+                    <option value="">-- Choose a class/cours --</option>
                     {classes.map(cls => (
                         <option key={cls.id} value={cls.id}>{cls.name}</option>
                     ))}
